33d4ea66fe1176427e6e1ddabe2b2f3733e7902d
Lisardo/Daniel <lisardo.kist@gmail.com>
2016-01-22 15:42:09 -0200

    Add activate user method.

--- control.cpp
+++ control.cpp
@@ -12,3 +12,14 @@ void hello_world()
 	printf("hello world");
 	// return 0;
 }
+
+void ActivateUser() {
+	activate_data_t user_act_data;
+	char key[] = "a4dc524ca61ca643a9cf1a70cc3f711cb17fc8dea13e7ef96d12bdc10ee2a11a";
+	user_act_data.app_key = key;
+	user_act_data.app_id = 1024717;
+	user_act_data.app_api_level = 2;
+	user_act_data.app_ver = SDK_VERSION;
+	DJI_Pro_Activate_API(&user_act_data, NULL);
+
+}
--- control.h
+++ control.h
@@ -9,6 +9,10 @@
 #include "DJI_LIB/DJI_Pro_Config.h"
 #include "DJI_LIB/DJI_Pro_Rmu.h"
 
+#define MAKE_VERSION(a,b,c,d) (((a << 24)&0xff000000) | ((b << 16)&0x00ff0000) | ((c << 8)&0x0000ff00) | (d&0x000000ff))
+#define SDK_VERSION           (MAKE_VERSION(2,3,10,0))
+
 void hello_world(void);
+void ActivateUser(void);
 
 #endif
