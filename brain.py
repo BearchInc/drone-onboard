bbe9a5596649adf13e21b65f544b67e8dba309bc
Diego Borges <drborges.cic@gmail.com>
2016-01-22 11:32:03 -0200

    Spike on rpi.gpio

---
+++ brain.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+while True:
+    takingoff_idle = GPIO.input(18)
+    landing_idle = GPIO.input(23)
+
+    if not takingoff_idle:
+        print('Takeoff Button Pressed')
+
+    if not landing_idle:
+        print('Landing Button Pressed')
+        time.sleep(0.2)
+
+    time.sleep(0.2)
