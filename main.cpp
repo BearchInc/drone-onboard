fa70593bbdd7bd65fe93733a08bee6412b9fd28e
lol <lol@gmail.com>
2016-01-25 13:52:09 +0000

    Fix button library

--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@
 #include <unistd.h>
 #include <string.h>
 #include "control.h"
+#include <wiringPi.h>
 
 
 
 int main(int argc,char **argv)
 {
 	const int butPin = 17;
-    wiringPiSetupGpio(); // Initialize wiringPi -- using Broadcom pin numbers
-    pinMode(butPin, INPUT);      // Set button as INPUT
+	bool button_pressed = false;
+    	wiringPiSetupGpio(); // Initialize wiringPi -- using Broadcom pin numbers
+    	pinMode(butPin, INPUT);      // Set button as INPUT
 
 	printf("Opening serial port...\n");
 	if (OpenSerialPort() < 0) {
@@ -22,7 +24,7 @@ int main(int argc,char **argv)
 		// Button is released if this returns 1
 		if (digitalRead(butPin)) { 
 			printf("Button released...\n");
-			button_pressed = false
+			button_pressed = false;
 		} else if(!button_pressed) {
 			printf("Activating user...\n");
 			printf("Button pressed...\n");
