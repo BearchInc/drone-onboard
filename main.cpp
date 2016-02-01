3fabbb4a8b5261946632bbc1cf6ff21aeccb5da9
Diego Borges <drborges.cic@gmail.com>
2016-02-01 11:53:15 -0200

    Link dji onboard sources as shared library

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <DJI_Pro_Hw.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    Pro_Hw_Setup("lol", 123);
+
+    cout << "Hello, World!" << endl;
+    return 0;
+}
